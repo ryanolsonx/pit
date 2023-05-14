0161b9b53bb52823231b7e19bf7c18e13d4ea2c9
Ryan Olson <ryolson@me.com>
2023-05-13 22:53:13 -0600

    Add hash-object and cat-file
    Test with:
    $ cd /tmp/new
    $ ugit init
    Initialized empty ugit repository in /tmp/new/.ugit
    $ echo some file > bla
    $ ugit hash-object bla
    0e08b5e8c10abc3e455b75286ba4a1fbd56e18a5
    $ ugit cat-file 0e08b5e8c10abc3e455b75286ba4a1fbd56e18a5
    some file

--- pit/cli.py
+++ pit/cli.py
@@ -1,18 +1,52 @@
 import argparse
+import hashlib
+import os
+import sys
+
+
+GIT_DIR = ".pit"
+
+
+def cat_file(args):
+    sys.stdout.flush()
+    with open(os.path.join(GIT_DIR, "objects", args.object), "rb") as f:
+        data = f.read()
+    sys.stdout.buffer.write(data)
+
+
+def hash_object(args):
+    with open(args.file, "rb") as f:
+        data = f.read()
+        oid = hashlib.sha1(data).hexdigest()
+    path = os.path.join(GIT_DIR, "objects", oid)
+    with open(path, "wb") as out:
+        out.write(data)
+    print(oid)
 
 
 def init(args):
-    print("Hello world")
+    cwd = os.getcwd()
+    os.makedirs(GIT_DIR)
+    os.makedirs(os.path.join(GIT_DIR, "objects"))
+    print(f"Initialized empty pit repository in {cwd}/{GIT_DIR}")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    cmds = parser.add_subparsers(dest='command')
+    cmds = parser.add_subparsers(dest="command")
     cmds.required = True
 
-    init_parser = cmds.add_parser('init')
+    init_parser = cmds.add_parser("init")
     init_parser.set_defaults(func=init)
+
+    hash_object_parser = cmds.add_parser("hash-object")
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument("file")
+
+    cat_file_parser = cmds.add_parser("cat-file")
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument("object")
 
     return parser.parse_args()
 
