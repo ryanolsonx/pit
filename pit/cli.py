acedf76d73270403b9e16f5bdbf4a88701cd0e94
Ryan Olson <ryolson@me.com>
2023-05-13 22:38:42 -0600

    Add argument parser

--- pit/cli.py
+++ pit/cli.py
@@ -1,2 +1,23 @@
-def main():
+import argparse
+
+
+def init(args):
     print("Hello world")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    cmds = parser.add_subparsers(dest='command')
+    cmds.required = True
+
+    init_parser = cmds.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    args.func(args)
+    
